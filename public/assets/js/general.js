8f38149bdf0fb703880cfa94d03ac62f784e0e57
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-03-10 20:36:18 -0400

    added animiation

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,7 +1,8 @@
 // Wrap every letter in a span
-
 var textWrapper = document.querySelector('.glow');
 var textContent = document.querySelector('.col-md-12')
+var fadeContainer = document.querySelector(".fade")
+var playerSelected = false
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 textContent.innerhtml = textWrapper.textContent.replace(/\S/g, "<span class='logo'>$&</span>");
 
@@ -42,5 +43,32 @@ anime.timeline({loop: false})
 
   });
 
+  anime.timeline({loop: false})
+  .add({
+    targets: '.fade',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 3000,
+    delay: (el, i) => 70*i
+  }).add({
+    targets: '.ml2',
+    opacity: 0,
+    duration: 3500,
+    easing: "easeOutExpo",
+    delay: 3500,
+
+  });
+
+  
+  if (playerSelected == true ){
+    $('.start-game').click(function(){
+        window.location.href='game.html';
+     })
+  }
   
   
+
+
+
