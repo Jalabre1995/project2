e484b7029a3a832b0d738a5b78d78dfe3b78fe01
Joshua Alabre <57151108+Jalabre1995@users.noreply.github.com>
2020-03-12 21:57:53 -0400

    Database

--- DB/schema.sql
+++ DB/schema.sql
@@ -3,7 +3,6 @@ CREATE DATABASE teamplayer;
 USE teamplayer;
 
 CREATE TABLE players(
-    id INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(20) DEFAULT NULL,
     lastName VARCHAR(20) DEFAULT NULL,
     teamName VARCHAR(30) DEFAULT NULL
