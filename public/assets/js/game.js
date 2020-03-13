56d74f7e58cca40fe8e7f86807475ccc28b490ac
Justin Durocher <justduro@gmail.com>
2020-03-12 20:23:54 -0400

    Ready for high scores

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -32,7 +32,7 @@ ball.click(shootFirstBall)
 
             } 
 
-        }, 2000);
+        }, 1000);
 
     }
 
@@ -90,7 +90,7 @@ ball.click(shootFirstBall)
 
             showSwoosh();
 
-        } else if (secondsLeft<50 && secondsLeft>48){
+        } else if (secondsLeft<48 && secondsLeft>46){
 
             showSwoosh();
 
@@ -129,12 +129,8 @@ ball.click(shootFirstBall)
         } else if (secondsLeft<3 && secondsLeft>1){
 
             showSwoosh();
-
-        } else if (secondsLeft<1) {
-
-            showSwoosh();
-
-        } 
+            
+        }
 
         $("#ball").animate({
 
@@ -174,55 +170,51 @@ ball.click(shootFirstBall)
 
             showSwoosh();
 
-        } else if (secondsLeft<54 && secondsLeft>52){
+        } else if (secondsLeft<53 && secondsLeft>51){
 
             showSwoosh();
 
-        } else if (secondsLeft<49 && secondsLeft>47){
+        } else if (secondsLeft<48 && secondsLeft>46){
 
             showSwoosh();
 
-        } else if (secondsLeft<44 && secondsLeft>42){
+        } else if (secondsLeft<43 && secondsLeft>41){
 
             showSwoosh();
 
-        } else if (secondsLeft<39 && secondsLeft>37){
+        } else if (secondsLeft<38 && secondsLeft>36){
 
             showSwoosh();
 
-        } else if (secondsLeft<34 && secondsLeft>32){
+        } else if (secondsLeft<33 && secondsLeft>31){
 
             showSwoosh();
 
-        } else if (secondsLeft<29 && secondsLeft>27){
+        } else if (secondsLeft<28 && secondsLeft>26){
 
             showSwoosh();
 
-        } else if (secondsLeft<24 && secondsLeft>22){
+        } else if (secondsLeft<23 && secondsLeft>21){
 
             showSwoosh();
 
-        } else if (secondsLeft<21 && secondsLeft>19){
+        } else if (secondsLeft<18 && secondsLeft>16){
 
             showSwoosh();
 
-        } else if (secondsLeft<16 && secondsLeft>14){
+        } else if (secondsLeft<13 && secondsLeft>11){
 
             showSwoosh();
 
-        } else if (secondsLeft<11 && secondsLeft>9){
+        } else if (secondsLeft<8 && secondsLeft>6){
 
             showSwoosh();
 
-        } else if (secondsLeft<6 && secondsLeft>4){
+        } else if (secondsLeft<3 && secondsLeft>1){
 
             showSwoosh();
 
-        } else if (secondsLeft<1) {
-
-            showSwoosh();
-
-        } 
+        }
 
         $(".ball").animate({
 
