b5d16c430ee17c25c999734217772129912cfa97
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-03-13 16:24:52 -0400

    added button to dropdown

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -2,23 +2,23 @@
 var textWrapper = document.querySelector('.glow');
 var textContent = document.querySelector('.col-md-12')
 var fadeContainer = document.querySelector(".fade")
-var dropbtn = $(".dropdown") 
-var divdrop = $("<div>") 
+var dropbtn = $(".dropdown")
+var divdrop = $("<div>")
 var link = $("<button>").text("link")
-var id  =  $("img").attr('id')
+var id = ""
 var playerSelected = false
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 textContent.innerhtml = textWrapper.textContent.replace(/\S/g, "<span class='logo'>$&</span>");
 
-anime.timeline({loop: false})
+anime.timeline({ loop: false })
   .add({
     targets: '.glow .letter',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 950,
-    delay: (el, i) => 70*i
+    delay: (el, i) => 70 * i
   }).add({
     targets: '.ml2',
     opacity: 0,
@@ -28,16 +28,16 @@ anime.timeline({loop: false})
 
   });
 
-  
-  anime.timeline({loop: false})
+
+anime.timeline({ loop: false })
   .add({
     targets: '.col-md-12',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 3000,
-    delay: (el, i) => 70*i
+    delay: (el, i) => 70 * i
   }).add({
     targets: '.ml2',
     opacity: 0,
@@ -47,15 +47,15 @@ anime.timeline({loop: false})
 
   });
 
-  anime.timeline({loop: false})
+anime.timeline({ loop: false })
   .add({
     targets: '.fade',
-    scale: [4,1],
-    opacity: [0,1],
+    scale: [4, 1],
+    opacity: [0, 1],
     translateZ: 0,
     easing: "easeOutExpo",
     duration: 3000,
-    delay: (el, i) => 70*i
+    delay: (el, i) => 70 * i
   }).add({
     targets: '.ml2',
     opacity: 0,
@@ -65,25 +65,28 @@ anime.timeline({loop: false})
 
   });
 
-  
-  if (playerSelected == true ){
-    $('.start-game').click(function(){
-        window.location.href='game.html';
-        
-     })
-  } else  {
-    console.log("yo");
-    $('.start-game').click(function(){
+
+if (playerSelected == true) {
+  $('.start-game').click(function () {
+    window.location.href = 'game.html';
+
+  })
+} else {
+
+  $('.start-game').click(function () {
     alert("please select player before starting game")
-    })
-  }
-  
-  
+  })
+}
+
+
 
 
 divdrop.addClass("dropdown-content")
 
 dropbtn.append(divdrop)
+
+
+
 
 /*function selected (){
   var startgame = 
@@ -104,33 +107,65 @@ $.ajax(settings).done(function (response) {
 	console.log(response);
 }); */
 
+$('img').click(function () {
+  id = ""
+  id = $(this).attr('id');
 
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-nba-v1.p.rapidapi.com/players/teamId/" +id,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-		"x-rapidapi-key": "48b0d11733msh462aed6ac396ee1p1555a7jsn0d010acc7eea"
-	}
-}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
-  var players  = [] 
-  for(var i = 0; i < response.api.players.length; i++){
-  players.push((response.api.players[i].firstName + " " + response.api.players[i].lastName + "<br>"  ))
+
+
+
+
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api-nba-v1.p.rapidapi.com/players/teamId/" + id,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+      "x-rapidapi-key": "48b0d11733msh462aed6ac396ee1p1555a7jsn0d010acc7eea"
+    }
   }
-  var link = $("<button>").text
-  console.log(players);
- // link.append(players)
-  divdrop.append(players)
+  console.log(id);
+
+  $.ajax(settings).done(function (response) {
+
+    console.log(response);
+
+    var players = []
+    for (var i = 0; i < response.api.players.length; i++) {
+      players.push((response.api.players[i].firstName + " " + response.api.players[i].lastName ))
+    }
+    //var link = $("<button>").text
+    console.log(players);
+    // link.append(players)
+    divdrop.empty()
+
+    for (var i = 0; i < players.length; i++) {
+      playbtn = $("<button/>").text(players[i])
+      playbtn.addClass("btn" , "btn-light", "players")
+      divdrop.append(playbtn)
+    }
     console.log(divdrop);
-    
-	dropbtn.append(divdrop);
-});
+    console.log(dropbtn);
+    $(".fade").css({ opacity: 0.0 });
+    $(".fade").css( "margin-top","2500px")
+    dropbtn.append(divdrop)
+      ;
+  });
 
+})
+/*
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
 
-
-
+    var i;
+    for (i = 0; i < divdrop.length; i++) {
+      var openDropdown = divdrop[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}*/
