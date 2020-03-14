c21e53b28392e8dba613eec9a0a9eb029fb5db18
Dameon Charleygit config --global user.email dameoncharley13@hotmail.comgit config --global user.name Dameon <dameoncharley13@hotmail.com>
2020-03-13 21:19:28 -0400

    able add data of player clicked on to page

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -4,6 +4,7 @@ var textContent = document.querySelector('.col-md-12')
 var fadeContainer = document.querySelector(".fade")
 var dropbtn = $(".dropdown")
 var divdrop = $("<div>")
+var playerDiv = $("<div>").addClass("playerInfo")
 var link = $("<button>").text("link")
 var id = ""
 var playerSelected = false
@@ -87,36 +88,11 @@ dropbtn.append(divdrop)
 
 
 
-
-/*function selected (){
-  var startgame = 
-}
-*/
-/*var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://api-nba-v1.p.rapidapi.com/teams/teamId/2",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-		"x-rapidapi-key": "48b0d11733msh462aed6ac396ee1p1555a7jsn0d010acc7eea"
-	}
-}
-
-$.ajax(settings).done(function (response) {
-	console.log(response);
-}); */
-
+function teamPlayer(playerSelected){
+//on click of logos imgs if fetches for players that play for that logo or team 
 $('img').click(function () {
   id = ""
   id = $(this).attr('id');
-
-
-
-
-
-
-
   var settings = {
     "async": true,
     "crossDomain": true,
@@ -127,45 +103,108 @@ $('img').click(function () {
       "x-rapidapi-key": "48b0d11733msh462aed6ac396ee1p1555a7jsn0d010acc7eea"
     }
   }
-  console.log(id);
+
 
   $.ajax(settings).done(function (response) {
-
-    console.log(response);
-
+    // takes data and push to an array using for a loop 
     var players = []
     for (var i = 0; i < response.api.players.length; i++) {
-      players.push((response.api.players[i].firstName + " " + response.api.players[i].lastName ))
+      players.push((response.api.players[i].firstName + " " + response.api.players[i].lastName))
     }
-    //var link = $("<button>").text
-    console.log(players);
-    // link.append(players)
-    divdrop.empty()
 
+
+    // clear div every time a new logo button is clicked to append new data from api 
+    divdrop.empty()
+    // loop to add players from api to a button then append button to a div 
     for (var i = 0; i < players.length; i++) {
       playbtn = $("<button/>").text(players[i])
-      playbtn.addClass("btn" , "btn-light", "players")
+      playbtn.addClass("btn")
+      playbtn.addClass("players")
+
+
       divdrop.append(playbtn)
     }
-    console.log(divdrop);
-    console.log(dropbtn);
+    //the class fade is faded and pushed down with added margin-top applied when the button is clicked allowing you to scroll through  the entire dropdown of players
     $(".fade").css({ opacity: 0.0 });
-    $(".fade").css( "margin-top","2500px")
+    $(".fade").css("margin-top", "2500px")
     dropbtn.append(divdrop)
+
+
+    //the user can then click on player to start game 
+    $('.players').click(function () {
+      var playerName = ""
+      playerName = $(this).text();
+      var firstName = playerName.split(" ", 1);
+
+
+
+
+
+
+// call to the api to search for  the player based on their first name 
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api-nba-v1.p.rapidapi.com/players/firstName/" + firstName,
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+          "x-rapidapi-key": "48b0d11733msh462aed6ac396ee1p1555a7jsn0d010acc7eea"
+        }
+      }
+
+      $.ajax(settings).done(function (response) {
+     
+            var info = []
+            playerDiv.empty()
+
+          // loop the data return from data and push to info array 
+        for (i = 0; i < response.api.players.length; i++) {
+         
+          
+          if (id = response.api.players[i].teamId) {
+          
+          console.log(id , response.api.players[i].collegeName);
+          //data being push is all the players with the same first name fix to only add player selected from earlier coming soon 
+            info.push(  '<div> Full Name: ' + playerName + '<div/>' +
+            '<div> College: ' + response.api.players[i].collegeName + '<div/>' +
+            ' <div>Country: ' + response.api.players[i].country + '<div/>' +
+            "<div>Date Of Birth: " + response.api.players[i].dateOfBirth +
+          "<div>  NBA Debut Year: " + response.api.players[i].startNba +'<div/>'
+         +"<div> Height in Meters: " + response.api.players[i].heightInMeters + '<div/>' +
+        " d<iv> Weight In Meters" +response.api.players[i].weightInKilograms +'<div/><br>' )
+         
+        }}   
+
+       
+        
+        playerDiv.append(info)
+        // player selected is turn to true allowing user to start game, if start button is clicked it the alert will still pop up fix coming soon  
+        playerSelected = true;
+        if (playerSelected == true) {
+          $('.start-game').click(function () {
+            window.location.href = 'game.html';
+          })
+        } 
+
+        // prepends data to the fade class also return its opacity to normal and margin-top to 0 when player button is selected 
+          $(".fade").prepend(playerDiv)
+          $(".fade").css({ opacity: 1.0 });
+          $(".fade").css("margin-top", "0px")
+        });
+
+    })
+
       ;
   });
 
-})
-/*
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
 
-    var i;
-    for (i = 0; i < divdrop.length; i++) {
-      var openDropdown = divdrop[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}*/
+})
+}
+teamPlayer()
+
+
+
+
+
+
